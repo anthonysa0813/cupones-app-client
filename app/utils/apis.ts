1296cf2f6c068b33dadd3d4726580da8547621b3
anthony <anthony0813@gmail.com>
2023-06-29 21:49:03 -0500

    uptated url server

--- app/utils/apis.ts
+++ app/utils/apis.ts
@@ -12,13 +12,16 @@ interface DataI {
 export const saveRegisterCoupon = async (data: DataI) => {
   try {
     // console.log(`http://localhost:5050/api/v1/user`);
-    const response = await fetch(`http://localhost:5050/api/v1/user`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
+    const response = await fetch(
+      `https://cupona-app-server-production.up.railway.app/api/v1/user`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    );
     const dataResponse = await response.json();
     console.log(dataResponse);
     return dataResponse;
