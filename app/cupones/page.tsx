3ee15f02b01ce7cabd273ddde74b98dcd4bfc379
anthony <anthony0813@gmail.com>
2023-07-11 15:15:44 -0500

    added new data fetching

--- app/cupones/page.tsx
+++ app/cupones/page.tsx
@@ -5,40 +5,49 @@ import { CuponI } from "../interfaces";
 import { generateExcelFile } from "../utils/generateXlsx";
 
 const CuponesPage = () => {
-  const [dataCupons, setDataCupons] = useState<CuponI[] | []>([]);
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
-    getInfo().then((info) => {
-      setDataCupons(info);
-    });
+    setLoading(true);
+    fetch("https://cupona-app-server-production.up.railway.app/api/v1/cupon")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.cupons);
+        setData(data.cupons);
+        setLoading(false);
+      });
   }, []);
 
-  const getInfo = async () => {
-    try {
-      console.log(`${process.env.NEXT_PUBLIC_DB_URL}cupon`);
-      const response = await fetch(`http://localhost:5050/api/v1/cupon`);
-      const data = await response.json();
-      return data.cupons;
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  if (isLoading) return <p>Loading...</p>;
+  if (!data) return <p>No profile data</p>;
 
-  const downloadFile = () => {};
+  // const getInfo = async () => {
+  //   try {
+  //     console.log(`${process.env.NEXT_PUBLIC_DB_URL}cupon`);
+  //     const response = await fetch(`http://localhost:5050/api/v1/cupon`);
+  //     const data = await response.json();
+  //     return data.cupons;
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  // const downloadFile = () => {};
 
   return (
     <div className="h-screen overflow-y-auto ">
-      <div className="mx-auto container mt-10 flex justify-between">
+      <div className="container flex justify-between mx-auto mt-10">
         <h1>Cupones</h1>
         <div className="">
-          <button onClick={() => generateExcelFile(dataCupons)}>
+          <button onClick={() => generateExcelFile(data)}>
             descargar cupones
           </button>
         </div>
       </div>
       <main>
         <div className="container mx-auto mt-10">
-          <TableList cupones={dataCupons} />
+          <TableList cupones={data} />
         </div>
       </main>
     </div>
